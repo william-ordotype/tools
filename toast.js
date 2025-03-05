4a46823ea01d91a96936ed09513a2a87380c060d
William <131350789+william-ordotype@users.noreply.github.com>
2025-03-05 14:13:22 +0100

    Create toast.js

---
+++ toast.js
@@ -0,0 +1,24 @@
+function c() {
+  // Select all trigger elements that have an attribute containing "show-toast"
+  document.querySelectorAll("[x-ordo-utils*=show-toast]").forEach(trigger => {
+    // Retrieve timeout value from data attribute or default to 3000ms
+    const timeout = trigger.dataset.showToastTimeout ?? 3000;
+    // Hide the common toast container initially
+    $(`[x-ordo-utils="toast-component-common"]`).hide();
+    
+    // Add click event listener to each trigger
+    trigger.addEventListener("click", event => {
+      event.preventDefault();
+      const toastElement = document.querySelector(`[x-ordo-utils="toast-component-common"]`);
+      if (toastElement) {
+        $(toastElement)
+          .css({ display: "inline-block", opacity: 0, top: -100 })
+          .animate({ top: 0, opacity: 1 }, 200);
+        setTimeout(function() {
+          $(toastElement).fadeOut(200);
+        }, timeout);
+      }
+    });
+  });
+}
+c();
